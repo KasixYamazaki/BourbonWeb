8bbdda7e591480287a9962f60d59e00263c510bb
KASIX Yamazaki <yamazaki@kasix.co.jp>
2025-07-16 15:00:46 +0900

    feat:スキャフォールディング

---
+++ BourbonWeb/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BourbonWeb.Models;
+
+namespace BourbonWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BourbonWeb.Models.Product> Product { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 4);
+        }
+    }
+}
