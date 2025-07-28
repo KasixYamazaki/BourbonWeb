31e4348afb6fccf923e20b170af6bf09d870c127
KASIX Yamazaki <yamazaki@kasix.co.jp>
2025-07-28 11:13:08 +0900

    feat:スキャフォールディング

---
+++ BourbonWeb/Models/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BourbonWeb.Models
+{
+    public class Sample
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Range(0, 10000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public required string Description { get; set; }
+    }
+}
