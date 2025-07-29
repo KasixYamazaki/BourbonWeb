a8016887bf920aa8d9d6e9d9bff072a16fc2e6fb
KASIX Yamazaki <yamazaki@kasix.co.jp>
2025-07-29 14:06:12 +0900

    feat:Sample.Descriptionを任意項目に変更

--- BourbonWeb/Migrations/20250729050210_UpdateSample.Description.Designer.cs
+++ BourbonWeb/Migrations/20250729050210_UpdateSample.Description.Designer.cs
@@ -3,6 +3,7 @@ using BourbonWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BourbonWeb.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250729050210_UpdateSample.Description")]
+    partial class UpdateSampleDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BourbonWeb/Models/Sample.cs
+++ BourbonWeb/Models/Sample.cs
@@ -12,7 +12,6 @@ namespace BourbonWeb.Models
         [Range(0, 10000)]
         public decimal Price { get; set; }
 
-        [Required]
-        public required string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
