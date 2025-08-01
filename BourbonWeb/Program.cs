19963e79cf75b5faa4cef2042e354181d955a884
KASIX Yamazaki <yamazaki@kasix.co.jp>
2025-08-01 15:55:28 +0900

    feat:Loginページ作成

--- BourbonWeb/Program.cs
+++ BourbonWeb/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Samples}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
